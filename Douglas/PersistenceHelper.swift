b8f9cfca3040b9bd32514b0ff41f343af05de6a0
NathanDevelops97 <you@example.com>
2016-10-01 19:38:11 -0400

    Got Core Data working for the AddTask function of the app

--- Douglas/PersistenceHelper.swift
+++ Douglas/PersistenceHelper.swift
@@ -6,4 +6,61 @@
 //  Copyright © 2016 Nathanael Grant. All rights reserved.
 //
 
-import Foundation
+
+import UIKit
+import CoreData
+
+class PersistenceHelper: NSObject {
+    
+    var appDel: AppDelegate = (UIApplication.shared.delegate as! AppDelegate)
+    var context: NSManagedObjectContext;
+    
+    override init(){
+        context = appDel.managedObjectContext
+    }
+    
+    func save(entity: String, parameters: Dictionary<String,String> )->Bool{
+        
+        let newEntity = NSEntityDescription.insertNewObject(forEntityName: entity, into: context)
+        for (key, value) in parameters{
+            newEntity.setValue(value, forKey: key)
+        }
+        
+        do {
+            try context.save()
+            return true
+        } catch _ {
+            return false
+        }
+    }
+    
+    func list(entity: String ) ->NSArray{
+        
+        let request:NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: entity)
+        request.returnsObjectsAsFaults = false;
+        let results: NSArray = try! context.fetch(request) as NSArray
+        return results
+    }
+    
+    func remove(entity:String, key:String, value:String)->Bool{
+        
+        let request:NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: entity)
+        request.returnsObjectsAsFaults = false
+        request.predicate = NSPredicate(format: "\(key) = %@", value)
+        let results: NSArray = try! context.fetch(request) as NSArray
+        
+        if(results.count>0){
+            
+            let res = results[0] as! NSManagedObject
+            context.delete(res)
+            do {
+                try context.save()
+            } catch _ {
+            }
+            return true
+        }
+        
+        return false
+    }
+    
+}
--- Douglas/TaskManager.swift
+++ Douglas/TaskManager.swift
@@ -6,4 +6,48 @@
 //  Copyright © 2016 Nathanael Grant. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+var taskMgr: TaskManager = TaskManager()
+
+struct AddTask {
+    var name = "Name"
+    var note = "Note"
+}
+
+class TaskManager: NSObject {
+    
+    var tasks = [AddTask]()
+    var persistenceHelper: PersistenceHelper = PersistenceHelper()
+    
+    override init(){
+        
+        let tempTasks:NSArray = persistenceHelper.list(entity: "Task")
+        for res:AnyObject in tempTasks as [AnyObject] {
+            tasks.append(AddTask(name:res.value(forKey: "name")as! String,note:res.value(forKey: "note") as! String))
+        }
+    }
+    
+    func addTask(_ name:String, note: String){
+        
+        var dicTask: Dictionary<String, String> = Dictionary<String,String>()
+        dicTask["name"] = name
+        dicTask["note"] = note
+        
+        
+        if(persistenceHelper.save(entity: "Task", parameters: dicTask)){
+            tasks.append(AddTask(name: name, note: note))
+        }
+    }
+    
+    func removeTask(_ index:Int){
+        
+        let value:String = tasks[index].name
+        
+        if(persistenceHelper.remove(entity: "Task", key: "name", value: value)){
+            tasks.remove(at: index)
+        }
+    }
+    
+    
+}
