19426278575bc7a90745f1643682be2036270f93
NathanDevelops97 <you@example.com>
2016-10-25 20:32:37 -0400

    Progress is progress

---
+++ ASCell.swift
@@ -0,0 +1,24 @@
+//
+//  ASCell.swift
+//  Douglas
+//
+//  Created by Nathanael Grant on 10/24/16.
+//  Copyright © 2016 Nathanael Grant. All rights reserved.
+//
+
+import UIKit
+
+class ASCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
